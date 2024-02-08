a905be358d91cff69ceb7fe4e3afec35b4a2c9ae
James_Muguro <kamandemuguro@gmail.com>
2024-02-08 14:01:06 +0300

    Updated brand_market_analysis.py with library imports and data loading functionality

--- brand_market_analysis.py
+++ brand_market_analysis.py
@@ -1,1 +1,40 @@
-# The Python Script will go here
+# To start working with the dataset, we will import the following libraries
+
+import pandas as pd  # Pandas for data manipulation and analysis
+import numpy as np   # NumPy for numerical operations
+import matplotlib.pyplot as plt  # Matplotlib for basic data visualization
+import seaborn as sns  # Seaborn for advanced data visualization
+from sklearn.cluster import KMeans  # KMeans for clustering
+from sklearn.preprocessing import StandardScaler  # StandardScaler for feature scaling
+from sklearn.model_selection import train_test_split  # Train-test split for model evaluation
+from sklearn.linear_model import LogisticRegression  # Logistic Regression for classification
+from sklearn.metrics import f1_score, confusion_matrix  # Metrics for model evaluation
+from sklearn.svm import SVC  # Support Vector Classifier for classification
+import ipywidgets as widgets
+from IPython.display import display, clear_output
+from tkinter import Tk, filedialog
+import numpy_financial as npf
+
+# Load the data
+
+def select_file(b):
+    clear_output()
+    root = Tk()
+    root.withdraw()  # Hide the main window
+    root.call('wm', 'attributes', '.', '-topmost', True)  # Raise the root to the top of all windows
+    b.files = filedialog.askopenfilename(multiple=False)  # List of selected files
+    path = b.files
+    global df
+    df = pd.read_excel(path)
+    print(f'Loaded dataframe from {path}')
+    display(df.head())
+
+fileselect = widgets.Button(description="File select")
+fileselect.on_click(select_file)
+
+display(fileselect)
+df.head()
+
+df.dtypes
+
+df.shape
